5a4b63230f445aad89b2e2546d3220f30f632ef2
soyocar <royomek@gmail.com>
2022-05-25 20:02:06 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import "./styles.css";
 let btnEnv = document.getElementById("enviar");
-btnEnv.addEventListener("click", () => {});
+let PrimerArray: number[] = new Array(6);
+let SegundoArray: number[] = new Array(6);
+let SumaArray: number[] = new Array(6);
+let indice: number;
+btnEnv.addEventListener("click", () => {
+  function CargarArray(arraycarga: number[]) {
+    for (indice = 0; indice < 6; indice++) {
+      arraycarga[indice] = prompt(
+        `Ingrese el valor de la posicion ${indice}: `
+      );
+    }
+  }
+});
